0ee0addeb7a573a0165728186eac12b6696a34da
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2021-10-27 18:56:27 +0100

    Update arduinoserialex2.ino

--- arduinoserialex2.ino
+++ arduinoserialex2.ino
@@ -1,14 +1,13 @@
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // put your setup code here, to run once:
+Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
+  // put your main code here, to run repeatedly:
+float data = analogRead(A0);
+data = (data/1024.00) * 5;
+String dataToSend = String(data);
+Serial.println(dataToSend);
+delay(300);
 }
