937b8edb625ff42622fb00718b02fffc7d190b7f
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2021-11-03 12:50:59 +0000

    Create bilateralcomex1.py

---
+++ bilateralcomex1.py
@@ -0,0 +1,27 @@
+import serial
+import time
+import numpy as np
+serialcomm = serial.Serial('/dev/ttyACM0', 9600)
+
+serialcomm.timeout = 1
+data=[]
+
+while True:
+
+    i = str(1.2)
+
+    if i == "Done":
+
+        print('finished')
+
+        break
+
+    serialcomm.write(i.encode())
+
+    time.sleep(0.1)
+    s=serialcomm.readline()#.decode('ascii')
+    for i in range(0,len(s)-1,2):
+        print(int(s[i])*10 + int(s[i + 1]))
+
+
+serialcomm.close()
